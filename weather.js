a9fe3e73898960d931eed10ec17afb88d9ee013b
Simon Wu <109928703+siwuagain@users.noreply.github.com>
2022-11-09 11:06:03 -0500

    separate stylesheets

--- weather.js
+++ weather.js
@@ -1,3 +1,22 @@
+getWeatherInfo('montreal');
+
+const locationInput = document.getElementById('location-input');
+const searchSubmit = document.getElementById('search');
+
+console.log(locationInput.value)
+
+searchSubmit.addEventListener('click', () => {
+  console.log(locationInput.value)
+  getWeatherInfo(locationInput.value)
+})
+
+locationInput.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    getWeatherInfo(locationInput.value)
+  }
+})
+
 async function getWeatherInfo(location) {
   
   const geocodingResponse = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=64206d65bc341529c8fea8f9158d50d4`, {mode: 'cors'});
@@ -8,15 +27,21 @@ async function getWeatherInfo(location) {
 
   console.log(geocodingData)
 
+  const locationName = document.getElementById('location-name')
+
+  locationName.textContent = `${geocodingData[0].name}, ${geocodingData[0].state}`
+
+
+
   const latCoord = geocodingData[0].lat;
 
   const lonCoord = geocodingData[0].lon;
 
   console.log(latCoord)
 
-  const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latCoord}&lon=${lonCoord}&exclude=minutely&appid=64206d65bc341529c8fea8f9158d50d4`, {mode: 'cors'});
+  const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latCoord}&lon=${lonCoord}&exclude=minutely&appid=64206d65bc341529c8fea8f9158d50d4&units=metric`, {mode: 'cors'});
 
-  const forecastResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latCoord}&lon=${lonCoord}&exclude=minutely&appid=64206d65bc341529c8fea8f9158d50d4`, {mode: 'cors'});
+  const forecastResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latCoord}&lon=${lonCoord}&exclude=minutely&appid=64206d65bc341529c8fea8f9158d50d4&units=metric`, {mode: 'cors'});
 
   const weatherData = await weatherResponse.json();
   
@@ -26,7 +51,13 @@ async function getWeatherInfo(location) {
 
   console.log(forecastData)
 
+  const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
   function convertUnix(unixTime, timezoneValue) {
+
+    const dateInfo = {}
 
     const dateFromUnix = new Date((unixTime + timezoneValue) * 1000)
 
@@ -40,53 +71,84 @@ async function getWeatherInfo(location) {
 
     //console.log(dateMinutes)
 
-    const timeOfRequest = dateHours.padStart(2, '0') + ':' + dateMinutes.padStart(2, '0');
+    dateInfo.timeofrequest = dateHours.padStart(2, '0') + ':' + dateMinutes.padStart(2, '0');
 
-    return timeOfRequest;
+    dateInfo.date = `${months[dateFromUnix.getMonth()]} ${dateFromUnix.getDate()}`
+    
+    dateInfo.dayofweek = weekdays[dateFromUnix.getDay()]
+
+    return dateInfo;
   }
 
 
 
   const showTime = document.getElementById('time');
 
-  showTime.textContent = `${convertUnix(weatherData.dt, weatherData.timezone)}`
+  showTime.textContent = `${convertUnix(weatherData.dt, weatherData.timezone).timeofrequest}`
 
-  const currentWeatherDisplay = document.getElementById('current-weather');
+  const currentWeather = document.getElementById('current-weather');
 
-  currentWeatherDisplay.textContent = `${weatherData.weather[0].main}`
+  currentWeather.textContent = `${weatherData.weather[0].main}`;
 
-  const currentWeatherDescription = weatherData.weather.description;
+  const currentWeatherDisplay = document.getElementById('current-weather-icon')
 
-  const currentTemp = weatherData.main.temp
+  console.log(currentWeatherDisplay)
+
+  currentWeatherDisplay.src = `images/${weatherData.weather[0].icon}.png`
+
+
+  const currentWeatherDescription = document.getElementById('current-weather-description');
+
+  console.log(currentWeatherDescription)
+  
+  currentWeatherDescription.textContent = `${weatherData.weather[0].description}`;
+
+  const currentTemp = document.getElementById('current-temp');
+
+  console.log(currentTemp)
+  
+  currentTemp.textContent = `${weatherData.main.temp} \u00B0C`
   
   console.log(currentTemp)
 
-  const feelsLikeTemp = weatherData.main.feels_like;
 
-  const currentHumidity = weatherData.main.humidity;
 
-  const windSpeed = weatherData.wind.speed;
+  const feelsLikeTemp = document.getElementById('feels-like')
+  
+  feelsLikeTemp.textContent = `Feels Like: \n ${weatherData.main.feels_like} \u00B0C`;
 
-  for (let i=0; i < forecastData.list.length; i+=8) {
+  const currentHumidity = document.getElementById('humidity');
+  
+  currentHumidity.textContent = `Humidity: \n ${weatherData.main.humidity} %`;
+
+  const windSpeed = document.getElementById('wind-speed');
+  
+  windSpeed.textContent = `Wind Speed: \n ${weatherData.wind.speed} m/s`;
+
+  const forecastDate = document.querySelectorAll('.forecast-date-info');
+
+  const maxTempOfForecastDay = document.querySelectorAll('.max-temp')
+
+  const minTempOfForecastDay = document.querySelectorAll('.min-temp')
+
+  const forecastWeatherDisplay = document.querySelectorAll('.forecast-weather-icon')
+
+  for (let i=0; i < 5; i++) {
+
+    const forecastDateInfo = convertUnix(forecastData.list[i*8].dt, weatherData.timezone)
+
     console.log(i)
-    console.log(convertUnix(forecastData.list[i].dt, weatherData.timezone))
 
-    const forecastMainTemp = forecastData.list[i].main.temp;
+    forecastDate[i].textContent = `${forecastDateInfo.dayofweek} \r\n ${forecastDateInfo.date} \r\n ${forecastDateInfo.timeofrequest}`
 
-    console.log(forecastMainTemp)
+    maxTempOfForecastDay[i].textContent = `${forecastData.list[i*8].main.temp_max} \u00B0C`
 
-    const forecastWeather = forecastData.list[i].weather[0].main
+    minTempOfForecastDay[i].textContent = `${forecastData.list[i*8].main.temp_min} \u00B0C`;
 
-    console.log(forecastWeather)
+    forecastWeatherDisplay[i].src = `images/${forecastData.list[i*8].weather[0].icon}.png`
   }
 
-  
-
-  
 }
 
-const locationInput = document.getElementById('city');
 
 
-
-getWeatherInfo('london')
